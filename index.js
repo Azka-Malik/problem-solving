96f5f7c60ebf31a116262fdb89cbf62202bbc5f0
Admin <Admin@USERTL6.tpsonline.com>
2022-11-01 14:00:18 +0500

    task1

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-const age = prompt("How old are you? ");
-console.log(`You are ${age} years old.`);
+//const age = prompt("How old are you? ");
+//console.log(`You are ${age} years old.`);
+
+Coins=[25,10,5,1]
+const amount=prompt("Amount: ");
+Result=0;
+remainder = 0;
+Count=0;
+i=0;
+    // Result = amount/Coins[i];
+     if(amount%Coins[i] == 0){
+        Result = amount / Coins[i];
+     }
+     else if (amount%Coins[i] != 0){
+        remainder = amount%Coins[i];
+        
+            n = remainder / Coins[i+1];
+            if(n==0){
+                Result= Result + n;
+            }
+
+     }
+   //return CoinReturn;   
+console.log(`Return Coins: ${Result}.`)
+
+
+
